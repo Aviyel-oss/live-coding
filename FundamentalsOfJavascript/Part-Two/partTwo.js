239948e11bafbbae5d02965bbcc2dac6f4b1fa4e
pramit-marattha <pramitmarattha5497@gmail.com>
2021-09-08 20:21:41 +0545

    conditional updated

---
+++ FundamentalsOfJavascript/Part-Two/partTwo.js
@@ -0,0 +1,49 @@
+// Conditionals
+// conditional statements controls the behaviour and determines wheather or not a piece of code can run or not
+
+// if- statement
+
+// if the expression is truthy then only the code executes
+
+// const isOnline = true;
+
+// if (isOnline) {
+//   console.log("You are connected to the internet");
+// }
+
+// else block executes if the "if" condition fails
+
+// const isOnline = true;
+
+// if (isOnline) {
+//   console.log("You are connected to the internet");
+// } else {
+//   console.log("You are not connected to the internet");
+// }
+
+// switch-case statements
+
+// the switch statement is used to perform different actions based upon different conditions
+
+// - switch is evaluated only once
+// - values of the expression arecompared with each case
+// - if there is a match the code block gets executed
+// if there is no match the default code block gets executed
+
+// const foobar = "barrrrrr";
+
+// switch (foobar) {
+//   case "foo": {
+//     let x = 20;
+//     console.log(x + 20);
+//     break;
+//   }
+//   case "bar": {
+//     let y = 30;
+//     console.log(y + 30);
+//     break;
+//   }
+//   default: {
+//     console.log("YESSSS!!!");
+//   }
+// }
